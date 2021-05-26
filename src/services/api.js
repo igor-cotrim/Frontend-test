8588bd16ad954477a40c5f82195ebcf1ce80b48a
Igor Cotrim <igorcotrim.dev@gmail.com>
2021-05-26 20:06:51 -0300

    feature: services/api

---
+++ src/services/api.js
@@ -0,0 +1,34 @@
+const baseURL = 'https://izap-frontend-challenges.herokuapp.com'
+
+export default async function post(data) {
+  try {
+    const response = await fetch(baseURL, {
+      method: 'post',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    if(response.status === 408){
+      return {
+        error: true,
+        errorMessage: 'Timeout Request'
+      }
+    } if(response.status === 500) {
+      return {
+        error: true,
+        errorMessage: 'Internal Server'
+      }
+    }
+    return {
+      error: false,
+      errorMessage: null,
+      data: await response.json()
+    }
+  } catch (error) {
+    return{
+      error: true,
+      errorMessage: error
+    }
+  }
+}
